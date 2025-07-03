4f016de1f981ffbbe3cfad4765a7188c9723d97d
unknown <bsuraj23@gmail.com>
2025-07-03 08:15:51 +0200

    my homeowrk

---
+++ Identity.py
@@ -0,0 +1,12 @@
+print("Identity opertors are IS and Is not ")
+a = "I am a"
+b = "I am b"
+c = 90
+print(type(a))
+print(type(b))
+print("a is b", a is b)
+print("a is not b", a is not b)
+print("a is c", a is c)
+print("b is c", b is c)
+if a is a:
+    print("I am inside loop")
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 some coments just to display branch copy 
 
 this comment vincet does not have ii eam expecting 
+dcnagdjhg
