8813f25fe96946335f8ac2dee359fe095350ca13
DeepaRaghu <rsumanthreddy03@gmail.com>
2025-07-06 17:45:18 +0530

    adding code

---
+++ type conversion.py
@@ -0,0 +1,18 @@
+#string to integer
+a="123"
+b=int(a)
+print(b+1)
+
+#integer to string
+c=123
+d=str(c)
+print(d+"1")
+#boolean to integer
+e=True
+f=int(e)
+print(f+1)
+#integer to boolean
+g=1
+h=bool(g)
+print(h)
+
