7b2ffbd49871ffac3460031d6d5446d16cf07c11
Harish Reddy <harishreddyr514@gmail.com>
2025-07-06 16:59:20 +0530

    pushing my code

---
+++ Type Conversions.py
@@ -0,0 +1,34 @@
+#Integer to String
+a=123
+b=str(a)
+print(b)
+print(type(b))
+
+#string to Integer
+a="123"
+b=int(a)
+print(b)
+print(type(b))
+
+#Boolean to Integer
+a=True
+c=int(a)
+print(c)
+print(type(c))
+
+#Integer to Boolean
+a=0
+b=bool(a)
+print(b)
+print(type(b))
+
+#String to Char
+string="Harish"
+char=string[3]
+print(char)
+print(type(char))
+
+#Character to Ascii
+char='A'
+ascii=ord(char)
+print(ascii)
