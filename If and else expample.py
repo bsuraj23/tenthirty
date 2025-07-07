49282258e6a2b33149b33c395b4dc080ee632e95
Sivaranjani-Siri <sivaranj54@gmail.com>
2025-07-07 12:34:24 +0530

    Add files via upload
    if and else example

---
+++ If and else expample.py
@@ -0,0 +1,10 @@
+name = "Siva"
+age = 25
+is_student = True
+print(name, age, is_student)
+print(name)
+print(age)
+if age >= 25:
+  print("im searching for job")
+else:
+  print("waiting for result")
