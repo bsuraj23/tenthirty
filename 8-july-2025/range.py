c30b4ae666a2cb6ce772401f104d26080f1746fe
MANI <mani@MANIs-MacBook-Air.local>
2025-07-08 20:04:19 +0530

    homeworks

---
+++ 8-july-2025/range.py
@@ -0,0 +1,17 @@
+#how to print odd numbers and even no's using range function ?
+
+for i in range(1,100,2):
+    print(i)
+
+
+for i in range(2,50,2):
+    print(i)
+
+
+#printing no's without -1 time ?
+
+for i in range(1,21+1):
+    print(i)
+
+for i in range(22)[1:]:
+    print(i)
