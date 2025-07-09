ad9a45fdcf0a03d8e60527615b07141e6acfc4e6
unknown <bsuraj23@gmail.com>
2025-07-09 08:03:37 +0200

    Adding control statement code

---
+++ Control Statements/3- for loop.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+
+
+
+ # Measure some strings:
+# words = ['mohan', 'sunil', 'uday']
+# for temp in words:
+#      print(temp)
+
+#cat 3
+#window 6
+#defenestrate 12
+
+
+
+#range([start], stop[, step])
+
+for i in range(1, 100, 4):
+    print(i)
+
+
+
---
+++ Control Statements/Triangle.py
@@ -0,0 +1,8 @@
+rows = 5
+for temp in range(1, rows + 1):        # 1 - 11
+    print("*"  * temp )
+
+
+
+
+
