37633512fa13761dca73f8806799a5ed75434161
Aj Kumar <aj@example.com>
2025-07-10 23:25:48 +0530

    my list.py

---
+++ list.py
@@ -0,0 +1,21 @@
+#creating a list
+list = [1,2,5,8,"sai","uday"]
+print(list)
+
+#printing index of above list
+print(list[0])
+print(list[-1])
+
+#using append for above list
+list.append("kumar")
+print(list)
+
+#using insert for above list
+list.insert(3,"welcome")
+print(list)
+
+#removing index for list
+list.remove(1)
+print(list)
+list = list * 2
+print(list)
