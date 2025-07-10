ea709ece487bec61c166431d14685d08f0f3e202
worldofvarn <varunchowdharyborra@gmail.com>
2025-07-10 23:20:39 +0530

    sashi list

---
+++ list sashi.py
@@ -0,0 +1,21 @@
+#creating a list
+list = [1,2,5,8,"sai","sashi"]
+print(list)
+
+#printing index of above list
+print(list[0])
+print(list[-1])
+
+#using append for above list
+list.append("kiran")
+print(list)
+
+#using insert for above list
+list.insert(3,"heyyy")
+print(list)
+
+#removing index for list
+list.remove(1)
+print(list)
+list = list * 2
+print(list)
