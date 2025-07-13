df2163af2b77313a01ff46d99d17cf118c155f4c
MANI <mani@MANIs-MacBook-Air.local>
2025-07-13 23:13:02 +0530

    union and intersection work

---
+++ 11-july-2025/discard.py
@@ -0,0 +1,8 @@
+#discard
+new_set = {1,2,3,4}
+new_set.discard(3)
+print(new_set)
+
+#remove 
+new_set.remove(2)
+print(new_set)
---
+++ 11-july-2025/intersection.py
@@ -0,0 +1,19 @@
+set1 = {77,78,79}
+set2 = {80,81,82}
+
+result = set1.intersection(set2)
+print(set2)
+
+result = set1 & set2
+print(result)
+
+
+#for union 
+
+
+result = set1 | set2
+print(result)
+
+
+result = set1.union(set2)
+print(result)
