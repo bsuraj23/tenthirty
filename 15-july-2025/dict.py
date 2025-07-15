fe74975c81e50b6e181e1ae13fd1735ac9fc23f2
MANI <mani@MANIs-MacBook-Air.local>
2025-07-15 21:37:17 +0530

    work is done

---
+++ 15-july-2025/dict.py
@@ -0,0 +1,20 @@
+dict1 = ["Name","age","college"]
+dict2 = {"Mani",21,"VITAP"}
+
+mydict = dict(zip(dict1,dict2))
+
+print(mydict)
+
+
+
+keys = ['a', 'b', 'c']
+values = [1, 2, 3]
+my_dic1t = dict(zip(keys, values))
+
+print(my_dic1t)
+
+
+names = ['Alice', 'Bob', 'Charlie']
+scores = [85, 92, 78]
+score_dict = dict(zip(names, scores))
+print(score_dict)
