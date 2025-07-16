d822c9728c21179bc2611583e7a7977fc0eed0bc
worldofvarn <varunchowdharyborra@gmail.com>
2025-07-16 22:24:36 +0530

    continue sashi

---
+++ sashi continue.py
@@ -0,0 +1,8 @@
+names=["navya" , "evangelin" , "sakshi" , "yashvi" , "janu" , "shazia"]
+
+for name in names:
+    if name in ["evangelin" , "yashvi"]:
+        continue
+    print(f"{name}; on going")
+    
+
