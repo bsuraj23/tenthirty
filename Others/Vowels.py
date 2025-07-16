635d1327f4051ebb09a9f5c9fb791c4246a96fcf
Sunil-sahoo19 <sunilsahoo2023@gmail.com>
2025-07-16 23:20:25 +0530

    Vowels file

---
+++ Others/Vowels.py
@@ -0,0 +1,6 @@
+text = "Nothing is permanent it written in Gita and Quran"
+values = "aeiouAEIOU"
+
+for char in text:
+    if char in values:
+        print(char)
--- Others/pass.py
+++ Others/pass.py
@@ -2,3 +2,4 @@ for i in range(5):
     if i == 3:
         pass
     print(i)
+
