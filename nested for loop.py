80166468cce71ab4eb713e320c20594003b4bfa8
Harish Reddy <harishreddyr514@gmail.com>
2025-07-16 22:30:36 +0530

    16th day codes

---
+++ nested for loop.py
@@ -0,0 +1,26 @@
+for over in range(1,11):
+    for balls in range(1,7):
+        print(over,"over",balls,"ball")
+
+#using break for loop
+print("Here we using break statement")
+for j in range(1,6):
+    if j==3:
+        break
+    print(j)
+
+
+#using continue for loop
+print("Here we using continue statement")
+for k in range(1,6):
+    if k==3:
+        continue
+    print(k)
+
+
+#using pass for loop
+print("Here we using pass statement")
+for l in range(1,6):
+    if l==3:
+        pass
+    print(l)
---
+++ printing vowels.py
@@ -0,0 +1,5 @@
+word=input("Enter the word:")
+vowels="AEIOUaeiou"
+for char in word:
+    if char in vowels:
+        print(char)
