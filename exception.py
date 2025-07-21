da6c8ec60779efe883a29da1b9ac4799ff200269
Sai Pavan Kumar <ssaipavan2003@gmail.com>
2025-07-21 22:15:26 +0530

    this file contains exceptions

---
+++ exception.py
@@ -0,0 +1,16 @@
+# try:
+#     int("abc")
+# except ValueError:
+#     print("invalid number format")
+
+
+# try:
+#     list = [1 , 2 , 3]
+#     print(list[5])
+# except IndexError:
+#     print("index out of range")
+
+try:
+    int("acd")
+    except Exception as m:
+print("we fount m",m)
