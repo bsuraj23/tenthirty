a878c88e584c73e42aefe5dd003b2ff17304b8da
Sunil-sahoo19 <sunilsahoo2023@gmail.com>
2025-07-21 23:32:23 +0530

    This file has Re.py

---
+++ Others/Re.py.py
@@ -0,0 +1,6 @@
+import re
+print(re.match(r'\d+','1589Hello'))
+print(re.match(r'\w+', 's##$')) 
+print(re.match(r'\w+','Hellooeveryone123'))
+print(re.match(r'[a-z]+','HEllOmiss'))
+print(re.match(r'[A-Z]\w+','Mr.SUnil'))
