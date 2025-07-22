5590ef81b162613d578c5bdfa86ec7af93650b2a
unknown <NAGAPURARCHANA98@GMAIL.COM>
2025-07-22 18:48:42 +0530

    logical

---
+++ logical/and.py
@@ -0,0 +1,4 @@
+a=True
+b=False
+result=a and b
+print("a and b=",result)
---
+++ logical/log.py
@@ -0,0 +1,13 @@
+a=true
+b=false
+result= a and b
+print("a and b =":,result)
+
+a=true
+b=false
+result= a or b
+print("a or b =":,result)
+
+ a=true
+result= not a
+print("not a =":,result)
