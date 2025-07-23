aa8de72e41699e28b083a0ca1913b2bf759efbf9
saadwitha <vsaadwitha@gmail.com>
2025-07-23 19:51:13 +0530

    built in functions

---
+++ built_functions.py
@@ -0,0 +1,40 @@
+#len
+name = "saadwitha"
+print(len(name))
+
+#type()
+x = 10
+print(type(x))
+
+#max and min
+numbers = [1,4,8,4,9]
+print(min(numbers))
+print(max(numbers))
+
+#sum
+marks = [12,6,33]
+print(sum(marks))
+
+#sorted
+numbers = [4,1,3,2]
+print(sorted(numbers))
+
+#input
+name = input("Enter your name:")
+print("Hello", name)
+
+#abs()
+print(abs(-10))
+
+#round
+print(round(3.1256, 2))
+
+#bool
+print(bool(0))
+print(bool("Hello"))
+
+#int(),string(),float()
+a = "130"
+print(int(a))
+print(str(a))
+print(float(a))
