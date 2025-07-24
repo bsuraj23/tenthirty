77e1752f34f19ca32c5b71e822ff8e43a4d3dde3
Sindhu635 <SINDHUJAALUKA@GMAIL.COM>
2025-07-24 20:06:48 +0530

    Regex

---
+++ .py
@@ -0,0 +1,18 @@
+# mymodule.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def greet(name):
+    return f"Hello, {name}!"
+
+# main.py
+
+import mymodule
+
+print(mymodule.add(5, 3))         
+print(mymodule.subtract(10, 4))   
+print(mymodule.greet("Sindhuja")) 
