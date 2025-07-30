d92edcc4720011ad558344316d154a7026017fcc
Udaykumarkotha <udaykumarkotha2003@gmail.com>
2025-07-30 23:17:31 +0530

    queue and dequeue

---
+++ queueanddequeue.py
@@ -0,0 +1,17 @@
+from queue import Queue
+q = Queue()
+q.put(789)
+q._put(2012)
+q.put(45)
+print(q.get())
+print(q.get())
+print(q.get())
+
+from collections import deque
+
+dq = deque()
+dq.appendleft(12)
+dq.append(56)
+dq.appendleft(58)
+print(dq.popleft())
+print(dq.pop())
