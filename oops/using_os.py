0c8c72eaa35f24e5856911cd6bc69c3ca30293a7
ChManish2003 <ch.manishkumar2003@gmail.com>
2025-07-30 23:01:27 +0530

    29-07-25 days codes

---
+++ oops/using_os.py
@@ -0,0 +1,12 @@
+import os
+
+#current directory
+print(os.getcwd())
+
+#list files in current directory
+print(os.listdir())
+# Example: Create a new directory
+#os.mkdir('txt_files')
+
+#remove a file
+os.remove('class_cr.py')  # Uncomment to remove a file
