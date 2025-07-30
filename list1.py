b8f7ba0f9dfa4d65052e2c253f69f46a522a68ca
Udaykumarkotha <udaykumarkotha2003@gmail.com>
2025-07-30 23:06:09 +0530

    uploading the list

---
+++ list1.py
@@ -0,0 +1,33 @@
+#creating a list
+<<<<<<< HEAD
+list = [1,2,5,8,"sai","pavan"]
+=======
+list = [1,2,5,8,"sai","uday"]
+>>>>>>> 37633512fa13761dca73f8806799a5ed75434161
+print(list)
+
+#printing index of above list
+print(list[0])
+print(list[-1])
+
+#using append for above list
+list.append("kumar")
+print(list)
+
+#using insert for above list
+<<<<<<< HEAD
+list.insert(3,"hello")
+=======
+list.insert(3,"welcome")
+>>>>>>> 37633512fa13761dca73f8806799a5ed75434161
+print(list)
+
+#removing index for list
+list.remove(1)
+print(list)
+list = list * 2
+<<<<<<< HEAD
+print(list)
+=======
+print(list)
+>>>>>>> 37633512fa13761dca73f8806799a5ed75434161
