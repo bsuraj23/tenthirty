d419530a920e2e23e5ab97d6d64a4b7aecd9f787
RelaDheeraj <reladheeraj00@gmail.com>
2025-07-30 21:51:04 +0530

    this contain queue and dqeue.

---
+++ queue and dqueue.py
@@ -0,0 +1,18 @@
+#example for queue.
+
+from queue import Queue
+q = Queue()
+q.put(69)
+q.put(96)
+print(q.get())  # 10
+print(q.get())  # 20
+
+
+#example for deque.
+
+from collections import deque
+dq = deque()
+dq.append(69)
+dq.appendleft(96)
+print(dq.pop())       # 10
+print(dq.popleft())   # 5
