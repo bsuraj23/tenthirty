9895dfb060511473dae0eb162a38b55bb0633f04
saadwitha <vsaadwitha@gmail.com>
2025-07-30 18:48:33 +0530

    priority queue

---
+++ PriorityQueue.py
@@ -0,0 +1,12 @@
+from queue import PriorityQueue
+
+# Create a PriorityQueue
+pq = PriorityQueue()
+
+# Add elements with priorities (priority, value)
+pq.put((2, "task low"))
+pq.put((1, "task medium"))
+pq.put((0, "task high"))
+
+print("Priority Queue Output:")
+
