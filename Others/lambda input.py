f1511731e4d56724e18685980cd0469ae21b2ea8
Sunil-sahoo19 <sunilsahoo2023@gmail.com>
2025-07-31 22:04:36 +0530

    Square without lambda , Lambda input

---
+++ Others/lambda input.py
@@ -0,0 +1,8 @@
+# Lambda function to square a number
+square = lambda x: x * x
+
+# Take user input
+num = int(input("Enter a number: "))
+
+# Call the lambda function
+print("Square is:", square(num))
---
+++ Others/square without lamda.py
@@ -0,0 +1,4 @@
+numbers = [1, 2, 3, 4, 5]
+squared = [x * x for x in numbers]
+
+print(squared)  # Output: [1, 4, 9, 16, 25]
