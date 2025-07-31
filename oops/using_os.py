c7accb76232e38e21d39cc1ae7da777fd15d1b52
ChManish2003 <ch.manishkumar2003@gmail.com>
2025-07-31 11:41:27 +0530

    dsa

--- oops/using_os.py
+++ oops/using_os.py
@@ -7,6 +7,6 @@ print(os.getcwd())
 print(os.listdir())
 # Example: Create a new directory
 #os.mkdir('txt_files')
-
+file= open('class_cr.py', 'w')
 #remove a file
 os.remove('class_cr.py')  # Uncomment to remove a file
