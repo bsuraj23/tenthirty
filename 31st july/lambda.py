f6a94ee020a1afbaae88a7b58986c3ce77e20704
Manojkumargamta <manuganta9908@gmail.com>
2025-07-31 22:11:20 +0530

    31st july homework

---
+++ 31st july/lambda.py
@@ -0,0 +1,8 @@
+# Lambda function to print user input
+print_input = lambda x: print(x)
+
+# Take input from the user
+user_input = input("Enter something: ")
+
+# Call the lambda function
+print_input(user_input)
