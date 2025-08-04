809adfcf42159bde1724b96385c81939e9d09b0a
worldofvarn <varunchowdharyborra@gmail.com>
2025-08-04 21:27:17 +0530

    using super

--- child object using parent  class.py
+++ child object using parent  class.py
@@ -1,11 +1,22 @@
+#class Animal:
+  #  def speak(self):
+ #       print("Animal speaks")
+
+#class Dog(Animal):
+  #  def speak(self):
+ #       print("Dog barks")
+
+# Polymorphism in action
+#def make_animal_speak(animal):
+ #   animal.speak()
+
+#using super
+
 class Animal:
     def speak(self):
         print("Animal speaks")
 
 class Dog(Animal):
     def speak(self):
-        print("Dog barks")
-
-# Polymorphism in action
-def make_animal_speak(animal):
-    animal.speak()
+        super().speak()      # Calls Animal's speak()
+        print("Dog barks")   # Adds Dog's specific behavior
