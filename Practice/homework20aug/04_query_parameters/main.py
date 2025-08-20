0b4fc24cec5bd5b2ce423ba10b9fd8a12e5435d5
DeepthiAddanki <deepthiiaddanki@gmail.com>
2025-08-20 16:42:47 +0530

    Fixed the error for search in 04_query parameter and pushed the code for get() in 05_pydantic request body

---
+++ Practice/homework20aug/04_query_parameters/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/search/")
+def search_items(q: str, limit: int = 10):
+    return {"query": q, "limit": limit}
+@app.get("/docs")
+def get_docs():
+    return {"docs_url": "http://127.0.0.1:8000/docs"}
