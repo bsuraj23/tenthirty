f04de016441215f48c08249bbcd0df83780e64a8
saadwitha <vsaadwitha@gmail.com>
2025-08-27 17:13:15 +0530

    query parameters

--- docforfun.py
+++ docforfun.py
@@ -1,21 +1,26 @@
+#fun that prints msg
 def say_hello():
     print("hello,welcome to python!")
 say_hello()
 
+#fun with parameters
 def greet(name):
     print("hello!",name)
 greet("saadwitha")
 
+#fun with return value
 def add(a,b):
     return a + b
 result = add(10,15)
 print("the sum is",result)
 
+#fun with default parameter
 def greet(name="guest"):
     print("Hello",name)
 greet("saadwitha")
 greet()
 
+#fun with multiple parameter
 def multiply(a,b):
     return a * b
 result = multiply(2,2)
