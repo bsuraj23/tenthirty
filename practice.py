aa04bde8a1090b62963ea2b59788aaddda926228
DeepaRaghu <rsumanthreddy03@gmail.com>
2025-09-10 11:57:41 +0530

    practiced codes

--- practice.py
+++ practice.py
@@ -1,4 +1,40 @@
+def greet():
+    print("hello world")
 
+#2
+def greet():
+    print("hello",name)
+    print("sumanth") 
+#3  
+def add(a,b):
+    return a+b
+    result=add(10,20)
+    print("sum:",result)
+# 4
+def get():
+    return[1,2,3,4,5]
+    print("never run")
+print(get_numbers())
+#22
+def numbers():
+    yield 20
+    yield 30
+    yield 40
+ gen=numbers()
+print(next(gen))
+print(next(gen))
+print(next(gen))
+    
+
+
+
+
+
+
+
+
+
+                                                         
 
 
 
