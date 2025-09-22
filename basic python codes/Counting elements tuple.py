a3346b25dafa9a15bd7d345393d9f259ba23f90a
Harish Reddy <harishreddyr514@gmail.com>
2025-09-22 15:46:02 +0530

    basic codes

--- basic python codes/Counting elements tuple.py
+++ basic python codes/Counting elements tuple.py
@@ -3,6 +3,7 @@ tuple=(1,2,4,5,32,7,9,3,5,7,8,36)
 frequencies=Counter(tuple)
 print(frequencies)
 
+#normal method
 tup=(1,2,4,5,32,7,9,3,5,7,8,36)
 freqs={}
 for item in tup:
---
+++ basic python codes/assert.py
@@ -0,0 +1,5 @@
+def square(x):
+    assert x >= 0, "X should be non-negative"
+    return x*x
+print(square(5))
+print(square(-5))
